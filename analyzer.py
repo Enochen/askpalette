140d475af0d1b3445221d2e3ca9f0d1d7d75d91c
Enoch Chen <sirenochen@gmail.com>
2019-09-21 13:41:43 -0400

    fix code

--- analyzer.py
+++ analyzer.py
@@ -5,4 +5,3 @@ response = google_images_download.googleimagesdownload()   #class instantiation
 keyword = "music"
 arguments = {"keywords":keyword,"limit":20,"no_download":True,"metadata":True}   #creating list of arguments
 paths = response.download(arguments)   #passing the arguments to the function
-return paths
