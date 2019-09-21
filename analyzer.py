b09b7ffec392d02a9fb8464397661b86dbad19fc
Enoch Chen <sirenochen@gmail.com>
2019-09-21 13:41:21 -0400

    base/sample project

---
+++ analyzer.py
@@ -0,0 +1,8 @@
+from google_images_download import google_images_download   #importing the library
+
+response = google_images_download.googleimagesdownload()   #class instantiation
+
+keyword = "music"
+arguments = {"keywords":keyword,"limit":20,"no_download":True,"metadata":True}   #creating list of arguments
+paths = response.download(arguments)   #passing the arguments to the function
+return paths
