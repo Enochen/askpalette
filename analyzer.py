cbb72d0c70eaf50f58b05f5bb3c6e9b9240d4272
Enoch Chen <sirenochen@gmail.com>
2019-09-21 14:34:12 -0400

    update requirements.txt

--- analyzer.py
+++ analyzer.py
@@ -1,7 +1,11 @@
 from google_images_download import google_images_download   #importing the library
 
-response = google_images_download.googleimagesdownload()   #class instantiation
+def stuff(keyword):
+    response = google_images_download.googleimagesdownload()   #class instantiation
+    arguments = {"keywords":keyword,"limit":20,"no_download":True,"metadata":True}   #creating list of arguments
+    paths = response.download(arguments)   #passing the arguments to the function
 
-keyword = "music"
-arguments = {"keywords":keyword,"limit":20,"no_download":True,"metadata":True}   #creating list of arguments
-paths = response.download(arguments)   #passing the arguments to the function
+    for i in paths[0][keyword]:
+        print(i)
+
+stuff("music")
